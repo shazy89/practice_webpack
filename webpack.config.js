47708e875f7921c823b23dfa93d55bab045ce425
shazy89 <erdo.shazy123@gmail.com>
2021-07-19 09:04:43 +0200

    wabpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+// path is from node js
+const path = require("path");
+
 const config = {
-  entry: "./src/index.js"
+  entry: "./src/index.js",
+  output: {
+    // in node js  __ is constant in node js and reference for current working directory
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  }
 };
 
 module.exports = config;
